41677e8993b9e061107cfca6d127ff85cfb34265
Silvio Traversaro <silvio@traversaro.it>
2024-01-08 15:25:37 +0100

    Add tests in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,4 +17,7 @@ cmake ${CMAKE_ARGS} $SRC_DIR \
       -DBUILD_SHARED_LIBS=ON
 
 make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make check
+fi
 make install
